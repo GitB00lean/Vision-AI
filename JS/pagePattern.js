b6671eb67f90f3c1ded462fcd2f14ec0ad92a2f0
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-07-03 22:32:49 +0530

    chore: Remove unused CSS styles and update homepage layout

---
+++ JS/pagePattern.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+  document.getElementById("wrapper").style.opacity = "1";
+  document.getElementById("container").style.opacity = "0";
+  setTimeout(function() {
+    document.getElementById("wrapper").style.opacity = "0";
+    document.getElementById("container").style.opacity = "1";
+  }, 3000);
+}
+
+const wrapper = document.getElementById("wrapper");
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const uniqueRand = (min, max, prev) => {
+  let next = prev;
+  
+  while(prev === next) next = rand(min, max);
+  
+  return next;
+}
+
+const combinations = [
+  { configuration: 1, roundness: 1 },
+  { configuration: 1, roundness: 2 },
+  { configuration: 1, roundness: 4 },
+  { configuration: 2, roundness: 2 },
+  { configuration: 2, roundness: 3 },
+
+];
+
+let prev = 0;
+
+setInterval(() => {
+  const index = uniqueRand(0, combinations.length - 1, prev),
+        combination = combinations[index];
+  
+  wrapper.dataset.configuration = combination.configuration;
+  wrapper.dataset.roundness = combination.roundness;
+  
+  prev = index;
+},1000);
